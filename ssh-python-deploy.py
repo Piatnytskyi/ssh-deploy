5639fce7bd06a2e0a69e8bbd228a30e408d0ddd4
Vitalii Piatnytskyi <v.v.piatnytskyi@gmail.com>
2023-08-13 15:55:44 +0300

    Made initial fixes for script

--- ssh-python-deploy.py
+++ ssh-python-deploy.py
@@ -1,7 +1,13 @@
 import sys
 import os
+import threading
 import paramiko
 import argparse
+
+def handle_stream(stream, label):
+    for line in stream:
+        print(f'{label}: {line.strip()}')
+        sys.stdout.flush()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Deploy and run a script on a remote host, along with its dependencies.')
@@ -9,13 +15,10 @@ if __name__ == '__main__':
     parser.add_argument('--username', help='Username for remote host', required=True)
     parser.add_argument('--password', help='Password for remote host', required=True)
     parser.add_argument('--script', help='Script to run on remote host', required=True)
-    parser.add_argument('--dependencies', help='Dependency files for the script', required=True, nargs='*')
+    parser.add_argument('--remote-path', help='Path on remote host to run a script on', required=True)
+    parser.add_argument('--dependencies', help='Dependency files for the script', required=False, nargs='*')
 
     args = parser.parse_args()
-
-    if not args.host or not args.username or not args.password or not args.script:
-        print("Please provide host, username, password, and script.")
-        sys.exit(1)
 
     # Connect to remote host
     client = paramiko.SSHClient()
@@ -24,16 +27,16 @@ if __name__ == '__main__':
 
     # Setup sftp connection and transmit the specified script and dependencies
     sftp = client.open_sftp()
-    remote_script = '/tmp/' + os.path.basename(args.script)
+    remote_script = args.remote_path + os.path.basename(args.script)
     sftp.put(args.script, remote_script)
     if args.dependencies:
         for dependency in args.dependencies:
-            sftp.put(dependency, '/tmp/' + os.path.basename(dependency))
+            sftp.put(dependency, args.remote_path + os.path.basename(dependency))
     sftp.close()
 
     # Run the transmitted script remotely without args and show its output.
     print(f"Executing {remote_script} ...")
-    stdout = client.exec_command(f'python {remote_script}')[1]
+    stdout = client.exec_command(f'python {remote_script} 2>&1')[1]
     for line in stdout:
         # Process each line in the remote output
         print(line.strip())
